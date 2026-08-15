42922711c52307d879c62eb1c98ecc14d7207003
Kamil <kmirosz6@gmail.com>
2026-08-15 13:48:20 +0200

    Implement custom user model and authentication serializer
    - Created a custom user model that uses email as the unique identifier instead of username.
    - Added a custom user manager to handle user creation and normalization.
    - Updated the Django admin to use a custom user admin interface.
    - Introduced a custom authentication serializer to authenticate users using email and password.
    - Enhanced user registration and authentication processes in the application.

--- user/models.py
+++ user/models.py
@@ -1,5 +1,33 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (
+    AbstractUser,
+    UserManager,
+)
+from django.db import models
+
+
+class CustomUserManager(UserManager):
+    def _create_user(self, email, password, **extra_fields):
+        # Normalize the email (lowercasing the domain part) …
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
 
 
 class User(AbstractUser):
-    pass
+    username = False
+    email = models.EmailField(unique=True)
+
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = "email"
+    objects = CustomUserManager()
